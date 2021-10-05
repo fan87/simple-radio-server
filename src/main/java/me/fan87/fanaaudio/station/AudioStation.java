e77618755b7dc524b0fb919bbf676fc1378be4c2
fan87 <fan87.TW@gmail.com>
2021-10-05 22:48:12 +0800

    Revert

--- src/main/java/me/fan87/fanaaudio/station/AudioStation.java
+++ src/main/java/me/fan87/fanaaudio/station/AudioStation.java
@@ -1,5 +1,8 @@
 package me.fan87.fanaaudio.station;
 
+import com.github.kokorin.jaffree.ffmpeg.FFmpeg;
+import com.github.kokorin.jaffree.ffmpeg.PipeInput;
+import com.github.kokorin.jaffree.ffmpeg.PipeOutput;
 import com.google.gson.annotations.Expose;
 import me.fan87.fanaaudio.FANARadio;
 import okhttp3.MediaType;
@@ -30,6 +33,7 @@ public class AudioStation {
     private transient long lastSentTime = System.currentTimeMillis();
     private transient Thread streamingThread = null;
     private transient Process process = null;
+    private transient boolean disableWatchdog = false;
 
     public transient List<File> tracks = new ArrayList<>();
     public transient Map<OutputStream, InetSocketAddress> receivers = new HashMap<>();
@@ -107,6 +111,72 @@ public class AudioStation {
     }
 
     /**
+     * Split a song into multiple parts to avoid FFmpeg glitch
+     * @param radio Radio instance
+     * @param track Track
+     */
+    private List<File> splitAudio(FANARadio radio, File track) {
+        radio.getLogger().info("Preparing Track: " + track.getName() + "! If it's stuck, it means the track wasn't ready. It will take a while, but it won't take too long.");
+        File cacheFolder = new File("cache/" + this.namespace + "/" + track.getName() + "/");
+        if (!cacheFolder.exists()) {
+            cacheFolder.mkdirs();
+        } else {
+            List<File> files = new ArrayList<>(Arrays.asList(cacheFolder.listFiles()));
+            files.sort(new Comparator<File>() {
+                @Override
+                public int compare(File o1, File o2) {
+                    int n1 = extractNumber(o1.getName());
+                    int n2 = extractNumber(o2.getName());
+                    return n1 - n2;
+                }
+
+                private int extractNumber(String name) {
+                    int i = 0;
+                    try {
+                        String number = name.split("_")[0];
+                        i = Integer.parseInt(number);
+                    } catch(Exception e) {
+                        i = 0;
+                    }
+                    return i;
+                }
+            });
+            return files;
+        }
+        ProcessBuilder builder = new ProcessBuilder("ffmpeg", "-i", String.format("%s", track.getAbsolutePath()), "-reset_timestamps", "1", "-y", "-f", "segment", "-segment_time", "300", "-acodec", "aac", "cache/" + this.namespace + "/" + track.getName() + "/%03d_out." + track.getName().split("\\.")[track.getName().split("\\.").length-1]); // Convert Format to streamable.aac (Using FFmpeg)
+        try {
+            builder.start().waitFor();
+
+            List<File> files = new ArrayList<>(Arrays.asList(cacheFolder.listFiles()));
+            files.sort(new Comparator<File>() {
+                @Override
+                public int compare(File o1, File o2) {
+                    int n1 = extractNumber(o1.getName());
+                    int n2 = extractNumber(o2.getName());
+                    return n1 - n2;
+                }
+
+                private int extractNumber(String name) {
+                    int i = 0;
+                    try {
+                        String number = name.split("_")[0];
+                        i = Integer.parseInt(number);
+                    } catch(Exception e) {
+                        i = 0;
+                    }
+                    return i;
+                }
+            });
+            return files;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+
+
+    }
+
+    /**
      * Start playing + processing tracks + sending radio data
      * @param radio Instance of FANARadio
      */
@@ -122,21 +192,26 @@ public class AudioStation {
                 for (File track : tracks) {
                     streamingThread = new Thread(() -> { // Streaming Thread. This will send a song.
                         try {
-                            radio.getLogger().info(String.format("[%s]  Started playing track: %s", this.namespace, track.getName()));
-                            ProcessBuilder builder = new ProcessBuilder("ffmpeg", "-re", "-i", String.format("%s", track.getAbsolutePath()), "-reset_timestamps", "1", "-ac", "2", "-y", "-f", "adts", "-acodec", "aac", "-sample_rate", "44100", "-map", "0:a", "-map_metadata", "-1", "-write_xing", "0", "-id3v2_version", "0", "-b:a", "256000", "pipe:"); // Convert Format to streamable.aac (Using FFmpeg)
-                            process = builder.start();
-                            InputStream inputStream = process.getInputStream();
-                            int read;
-                            while ((read = inputStream.read()) != -1) {
-                                this.lastSentTime = System.currentTimeMillis(); // For watchdog thread (Skip the song if it's stuck)
-                                sendData(radio, (byte) read); // Send data
-                            }
-                            if (radio.getConfigsManager().getConfig().debug) {
-                                Scanner errorScanner = new Scanner(process.getErrorStream());
-                                while (errorScanner.hasNextLine()) {
-                                    System.out.println(errorScanner.nextLine()); // Send ffmpeg output to console for debugging purpose
+                            disableWatchdog = true;
+                            for (File file : splitAudio(radio, track)) {
+                                disableWatchdog = false;
+                                radio.getLogger().info(String.format("[%s]  Started playing track: %s", this.namespace, track.getName()));
+                                ProcessBuilder builder = new ProcessBuilder("ffmpeg", "-re", "-i", String.format("%s", file.getAbsolutePath()), "-reset_timestamps", "1", "-ac", "2", "-y", "-f", "adts", "-acodec", "aac", "-sample_rate", "44100", "-map", "0:a", "-map_metadata", "-1", "-write_xing", "0", "-id3v2_version", "0", "-b:a", "256000", "pipe:"); // Convert Format to streamable.aac (Using FFmpeg)
+                                process = builder.start();
+                                InputStream inputStream = process.getInputStream();
+                                int read;
+                                while ((read = inputStream.read()) != -1) {
+                                    this.lastSentTime = System.currentTimeMillis(); // For watchdog thread (Skip the song if it's stuck)
+                                    sendData(radio, (byte) read); // Send data
+                                }
+                                if (radio.getConfigsManager().getConfig().debug) {
+                                    Scanner errorScanner = new Scanner(process.getErrorStream());
+                                    while (errorScanner.hasNextLine()) {
+                                        System.out.println(errorScanner.nextLine()); // Send ffmpeg output to console for debugging purpose
+                                    }
                                 }
                             }
+
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
@@ -144,9 +219,9 @@ public class AudioStation {
 
                     try {                     // Start song sending (Stream Thread) thread
                         streamingThread.start();
-                        while (!streamingThread.isInterrupted() && streamingThread.isAlive())
+                        while (!streamingThread.isInterrupted() && streamingThread.isAlive()) {}
                         radio.getLogger().warn(String.format("[%s]  Streaming Thread has been stopped! Song skipped!", namespace));
-                        if (radio.getConfigsManager().getConfig().debug) {
+                        if (radio.getConfigsManager().getConfig().debug && process != null) {
                             Scanner errorScanner = new Scanner(process.getErrorStream());
                             while (errorScanner.hasNextLine()) {
                                 System.out.println(errorScanner.nextLine()); // Send ffmpeg output to console for debugging purpose
@@ -156,9 +231,11 @@ public class AudioStation {
                         radio.getLogger().warn(String.format("[%s]  Streaming Thread has been stopped! Song skipped!", namespace));
                         if (radio.getConfigsManager().getConfig().debug) {
                             e.printStackTrace();
-                            Scanner errorScanner = new Scanner(process.getErrorStream());
-                            while (errorScanner.hasNextLine()) {
-                                System.out.println(errorScanner.nextLine()); // Send ffmpeg output to console for debugging purpose
+                            if (process != null) {
+                                Scanner errorScanner = new Scanner(process.getErrorStream());
+                                while (errorScanner.hasNextLine()) {
+                                    System.out.println(errorScanner.nextLine()); // Send ffmpeg output to console for debugging purpose
+                                }
                             }
                         }
                     }
@@ -168,6 +245,9 @@ public class AudioStation {
 
         new Thread(() -> {
             while (true) {
+                if (disableWatchdog) {
+                    lastSentTime = System.currentTimeMillis();
+                }
                 long timeout = System.currentTimeMillis() - lastSentTime;
                 if ((timeout) > 10000) {
                     lastSentTime = System.currentTimeMillis();
@@ -201,8 +281,8 @@ public class AudioStation {
             if (exchange.getRequestURI().getPath().endsWith(".m3u")) {
                 String text =
                         "#EXTM3U\n" +
-                        "#EXTINF:0, " + owner + " - " + name + "\n" +
-                        "radio.aac";
+                                "#EXTINF:0, " + owner + " - " + name + "\n" +
+                                "radio.aac";
                 exchange.getResponseHeaders().add("Content-Type", "audio/mpegurl");
                 exchange.sendResponseHeaders(200, text.getBytes(StandardCharsets.UTF_8).length);
                 exchange.getResponseBody().write(text.getBytes(StandardCharsets.UTF_8));
@@ -220,8 +300,8 @@ public class AudioStation {
                     "</head>\n" +
                     "<body>\n" +
                     "<h1>%s</h1>\n", radio.getConfigsManager().getConfig().stationName) +
-                    "<h2>Station not found! Here's all available stations:</h2>\n" +
-                    "<p>Note: M3U requires 3rd party software. If you want to play it in your browser, use AAC</p>\n");
+                    String.format("<h2>%s</h2>\n", radio.getConfigsManager().getConfig().radioIndexSubtitle) +
+                    String.format("<p>%s</p>\n", radio.getConfigsManager().getConfig().radioIndexHint));
             for (AudioStation station : radio.getStationsManager().stations) {
                 builder.append(String.format("\n<li><a href=\"%s\">%s</a>  (<a href=\"%s\">M3U</a> | <a href=\"%s\">AAC</a>) </li>",
                         "/" + station.namespace + "/radio.aac", station.name, "/" + station.namespace + "/radio.m3u", "/" + station.namespace + "/radio.aac"));
